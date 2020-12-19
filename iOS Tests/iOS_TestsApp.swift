65c77c6a3ae11e795a519d558675b21c5a0cb7a5
Joao Miranda <joao@streethawk.co>
2020-12-19 13:23:53 +1100

    Initial Commit

---
+++ iOS Tests/iOS_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TestsApp.swift
+//  iOS Tests
+//
+//  Created by Joao Miranda on 19/12/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
